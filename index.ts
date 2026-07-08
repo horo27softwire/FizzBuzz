336e62b57952c70e9b7dc7f2e91315b3a24cd440
horo27softwire <valentin.hororoi@softwire.com>
2026-07-08 12:10:37 +0300

    feat: part 2 fizbuzz

--- index.ts
+++ index.ts
@@ -1,19 +1,51 @@
 // This is our main function
-function fizzbuzz(): void {
-    for (let i = 1; i <= 100; i++){
-        if (i % 15 === 0) {
-            console.log("FizzBuzz");
-            continue;
+function fizzbuzz(numbersToCount : number): void {
+    for (let currentNumber : number = 1; currentNumber <= numbersToCount; currentNumber++){
+
+        let result: string[] = []
+
+        if (currentNumber % 3 === 0) {
+            result.push("Fizz");
         }
-        if (i % 3 === 0) {
-            console.log("Fizz");
-            continue;
+        if (currentNumber % 5 === 0) {
+            result.push("Buzz");
         }
-        if (i % 5 === 0) {
-            console.log("Buzz");
+
+        if (currentNumber % 7 === 0) {
+            result.push("Bang");
+        }
+
+        if (currentNumber % 11 === 0) {
+            result.length = 0
+            result.push("Bong");
+        }
+
+        if (currentNumber % 13 === 0) {
+            let index :number = result.length;
+            for (let resultElement: number = 0; resultElement < result.length; resultElement++) {
+                if (result[resultElement][0] == "B"){
+                    index = resultElement;
+                    break;
+                }
+            }
+
+            if (index == result.length){
+                result.push("Fezz");
+            }
+            else {
+                result.splice(index, 0, "Fezz");
+            }
+        }
+
+        if (currentNumber % 17 === 0) {
+            result.reverse();
+        }
+
+        if (result.length) {
+            console.log(result.join(""));
         }
     }
 }
 
 // Now, we run the main function:
-fizzbuzz();
+fizzbuzz(100);
