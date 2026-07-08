86b827ad057868cb7b920ea8d32b703b70486330
horo27softwire <valentin.hororoi@softwire.com>
2026-07-08 11:29:45 +0300

    feat: added fizzbuzz

--- index.ts
+++ index.ts
@@ -1,7 +1,18 @@
 // This is our main function
 function fizzbuzz(): void {
-    console.log("Hello, World!");
-    // Put your code here...
+    for (let i = 1; i <= 100; i++){
+        if (i % 15 === 0) {
+            console.log("FizzBuzz");
+            continue;
+        }
+        if (i % 3 === 0) {
+            console.log("Fizz");
+            continue;
+        }
+        if (i % 5 === 0) {
+            console.log("Buzz");
+        }
+    }
 }
 
 // Now, we run the main function:
